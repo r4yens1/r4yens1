ba48a4be7640eb48a6f176141d59a587333e3bb5
r4yens1 <naufaland79@gmail.com>
2025-10-28 11:27:31 +0700

    Create scipt.js

---
+++ scipt.js
@@ -0,0 +1,7 @@
+// 1. pilih tombol berdasaarkan ID-nya
+const tombolSapa = document.getElementById('sapa Button')
+// 2. Tambahkan 'event listener' untuk aksi 'click'
+tombolSapa.addEventListenner('click', function)
+// 3. Jalankan kode ini ketika tombol di-klik
+alert('halo! Terimakasih sudah berkunjung!');
+});
